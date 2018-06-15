264585c5b317dad2083483f7dbc0e1c1b8cbf27a
Siri Persson <siri.persson@sylog.se>
2018-06-15 09:36:57 +0100

    Refactor code into functions

--- inc/hangman.h
+++ inc/hangman.h
@@ -12,11 +12,19 @@
 
 using namespace std;
 
-const int MAXLENGTH=80;
+const int MAXLENGTH = 80;
 const int MAX_TRIES = 12;
 
-int letterFill (char, char[], char[]);
-void initializeUnknownWordWithStars (char[], char[]);
-void createHangmanGame ();
+int letterFill( char, char[], char[] );
+void initializeUnknownWordWithStars( char[], char[] );
+void startHangmanGame();
+int getRandomNumber();
+void welcomeTheUser( std::string playername );
+char getGuessedLetterFromUser( char unknown_word [] );
+bool checkIfAlreadyGuessedLetter( char guessed_letter, std::string already_guessed_letters );
+bool isAlphabeticalLetter( char guessed_letter );
+void displayNumberOfTriesLeft( int number_of_tries );
+void displayAlreadyGuessedLetters( std::string already_guessed_letters );
+void incrementNumberOfTriesAndAlreadyGuesssedLetters( char guessed_letter, std::string already_guessed_letters, int number_of_tries );
 
 #endif // __MYCLASS_H_INCLUDED__
--- src/hangman.cpp
+++ src/hangman.cpp
@@ -8,15 +8,17 @@
 #include "hangman.h"
 
 int main (){
-	createHangmanGame();
+	startHangmanGame();
 	return 0;
 }
 
-void createHangmanGame () {
+void startHangmanGame () {
 	std::string playername;
 	std::string already_guessed_letters;
-	std::size_t found;
 	int number_of_tries = 0;
+	int random_number;
+	bool alreadyGuessedLetter;
+	bool alphabeticalLetter;
 	char guessed_letter;
 	char unknown_word [MAXLENGTH];
 	char word[MAXLENGTH];
@@ -33,59 +35,60 @@ void createHangmanGame () {
 		"singapore"
 	};
 
-	srand(time(NULL));
-	int random_number = rand() % 10 +1;
+	random_number = getRandomNumber();
 	strcpy(word, allWords[random_number]);
 
 	initializeUnknownWordWithStars(word, unknown_word);
-
-	cout << "\nType your name: " << endl;
-	cin >> playername;
-	cout << "\nWelcome " << playername << " to this awesome Hangman game!" << endl;
-	cout << "Guess a country name."<< endl;
-	cout << "Each letter is represented by a star. You have to type only one letter in one try"<< endl;
-	cout << "You have " << MAX_TRIES << " tries to try and guess the word"<< endl;
+	welcomeTheUser(playername);
 
 	while (number_of_tries < MAX_TRIES) {
-		cout << unknown_word << endl;
-		cout << "\nGuess a letter: " << endl;
-		cin >> guessed_letter;
-		guessed_letter = tolower(guessed_letter);
+		guessed_letter= getGuessedLetterFromUser(unknown_word);
+		alreadyGuessedLetter = checkIfAlreadyGuessedLetter(guessed_letter, already_guessed_letters);
+		alphabeticalLetter = isAlphabeticalLetter(guessed_letter);
 
-		std::string string_letter;
-		stringstream ss;
-		ss << guessed_letter;
-		ss >> string_letter;
-		found=already_guessed_letters.find(string_letter);
-
-		if (found!=std::string::npos){
+		if (alreadyGuessedLetter)
+		{
 			cout << endl << "You have already guessed this letter. Try again!" << endl;
+			displayNumberOfTriesLeft(number_of_tries);
+			displayAlreadyGuessedLetters (already_guessed_letters);
 		}
 
-		if( isalpha(guessed_letter) == 0 && found == std::string::npos){
+		if( !alphabeticalLetter)
+		{
 			cout << endl << "You must enter an alphabetical letter. Try again!" << endl;
-		} else if (found == std::string::npos){
+			displayNumberOfTriesLeft(number_of_tries);
+			displayAlreadyGuessedLetters (already_guessed_letters);
+		}
+
+		else if ( !alreadyGuessedLetter )
+		{
 			already_guessed_letters = already_guessed_letters + guessed_letter + " ";
 			number_of_tries++;
 		}
 
-		if ( letterFill(guessed_letter, word, unknown_word)==0  && isalpha(guessed_letter) != 0 && found == std::string::npos ) {
+		if ( letterFill(guessed_letter, word, unknown_word) == 0  && alphabeticalLetter && !alreadyGuessedLetter )
+		{
 			cout << endl << "Whoops! That letter isn't in there!" << endl;
-			cout << "You have " << MAX_TRIES - number_of_tries << " guesses left." << endl;
-			cout << "Already guessed letters: " << already_guessed_letters <<endl;
+			displayNumberOfTriesLeft( number_of_tries );
+			displayAlreadyGuessedLetters ( already_guessed_letters );
 		}
-		else if (isalpha(guessed_letter) != 0 && found == std::string::npos){
+		else if ( alphabeticalLetter && !alreadyGuessedLetter )
+		{
 			cout << endl << "You found a letter! Isn't that exciting!" << endl;
-			if (strcmp(word, unknown_word) == 0) {
+			if ( strcmp( word, unknown_word ) == 0)
+			{
 				cout << word << endl;
 				cout << "\nCongratulations " << playername <<", you won the game!" << endl;
 				break;
-			} else {
-				cout << "You have " <<MAX_TRIES - number_of_tries << " guesses left." << endl;
-				cout << "Already guessed letters: " << already_guessed_letters <<endl;
+			}
+			else
+			{
+				displayNumberOfTriesLeft( number_of_tries );
+				displayAlreadyGuessedLetters ( already_guessed_letters );
 			}
 		}
-		if (number_of_tries == MAX_TRIES && isalpha(guessed_letter) != 0 && found == std::string::npos ) {
+		if ( number_of_tries == MAX_TRIES && alphabeticalLetter && !alreadyGuessedLetter )
+		{
 			cout << "\nSorry " << playername << ", you lose...you've been hanged." << endl;
 			cout << "The word was : " << word << endl;
 			break;
@@ -94,19 +97,78 @@ void createHangmanGame () {
 	getch(); // måste ha denna annars funkar inte inläggningen av gissningar
 }
 
-
-void initializeUnknownWordWithStars (char word[], char unknown_word[]) {
-	int i;
-	int wordlength = strlen(word);
-	for (i = 0; i < wordlength; i++)
-		unknown_word[i]='*';
-	unknown_word[i]='\0';
+int getRandomNumber()
+{
+	srand( time(NULL) );
+	int random_number = rand() % 10 + 1;
+	return random_number;
 }
 
-int letterFill (char guess, char secretword[], char guessword[]) {
+void welcomeTheUser( std::string playername )
+{
+	cout << "\nType your name: " << endl;
+	cin >> playername;
+	cout << "\nWelcome " << playername << " to this awesome Hangman game!" << endl;
+	cout << "Guess a country name."<< endl;
+	cout << "Each letter is represented by a star. You have to type only one letter in one try"<< endl;
+	cout << "You have " << MAX_TRIES << " tries to try and guess the word"<< endl;
+}
+
+char getGuessedLetterFromUser( char unknown_word [] )
+{
+	char guessed_letter;
+	cout << unknown_word << endl;
+	cout << "\nGuess a letter: " << endl;
+	cin >> guessed_letter;
+	return tolower( guessed_letter );
+}
+
+bool checkIfAlreadyGuessedLetter( char guessed_letter, std::string already_guessed_letters )
+{
+	std::string string_letter;
+	stringstream ss;
+	std::size_t found;
+	ss << guessed_letter;
+	ss >> string_letter;
+	found = already_guessed_letters.find( string_letter );
+	return found != std::string::npos;
+}
+
+bool isAlphabeticalLetter( char guessed_letter )
+{
+	return isalpha( guessed_letter ) != 0;
+}
+
+void displayNumberOfTriesLeft( int number_of_tries )
+{
+	cout << "You have " << MAX_TRIES - number_of_tries << " guesses left." << endl;
+}
+
+void displayAlreadyGuessedLetters( std::string already_guessed_letters )
+{
+	cout << "Already guessed letters: " << already_guessed_letters << endl;
+}
+
+void incrementNumberOfTriesAndAlreadyGuesssedLetters( char guessed_letter, std::string already_guessed_letters, int number_of_tries )
+{
+	already_guessed_letters = already_guessed_letters + guessed_letter + " ";
+	number_of_tries++;
+}
+
+void initializeUnknownWordWithStars( char word[], char unknown_word[] )
+{
+	int i;
+	int wordlength = strlen( word );
+	for( i = 0; i < wordlength; i++ )
+		unknown_word[i] = '*';
+	unknown_word[i] = '\0';
+}
+
+int letterFill( char guess, char secretword[], char guessword[] )
+{
 	int matches=0;
-	for (int i = 0; secretword[i]!='\0'; i++) {
-		if (guess == secretword[i]) {
+	for( int i = 0; secretword[i] != '\0'; i++ ) {
+		if ( guess == secretword[i] ) {
 			guessword[i] = guess;
 			matches++;
 		}
